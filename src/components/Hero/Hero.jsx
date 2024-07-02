f6d13b70de02e9cf6923415dc608e16a41b9c885
Maite Baida <maitemsabaida@gmail.com>
2024-07-02 15:26:15 -0600

    hero styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ function Hero() {
         <p className="hero__name">MAITE</p>
         <p className="hero__name hero__name--green">BAIDA</p>
       </div>
-      <div>
+      <div className="hero__subtitle-box">
         <p className="hero__subtitle hero__subtitle--green">PORTFOLIO</p>
         <p className="hero__subtitle">FULL-STACK DEVELOPER</p>
       </div>
