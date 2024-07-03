c2728f13966bb0542c059dfe9036ff7d184811a0
Maite Baida <maitemsabaida@gmail.com>
2024-07-03 15:41:00 -0600

    adjustments to header and footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,40 @@ import "./Header.scss";
 function Header() {
   const [selectedItem, setSelectedItem] = useState(" ");
 
+  const handleSelectedItem = (link) => {
+    setSelectedItem(link);
+  };
+
   return (
     <header className="header">
       <nav>
         <ul className="header__list">
-          <a href="#about">
-            <li className="header__item">About</li>
+          <a href="#about" onClick={() => handleSelectedItem("about")}>
+            <li
+              className={`header__item ${
+                selectedItem === "about" ? "header__item--selected" : " "
+              }`}
+            >
+              About
+            </li>
           </a>
-          <a href="#projects">
-            <li className="header__item">Projects</li>
+          <a href="#projects" onClick={() => handleSelectedItem("projects")}>
+            <li
+              className={`header__item ${
+                selectedItem === "projects" ? "header__item--selected" : " "
+              }`}
+            >
+              Projects
+            </li>
           </a>
-          <a href="#education">
-            <li className="header__item">Education</li>
+          <a href="#education" onClick={() => handleSelectedItem("education")}>
+            <li
+              className={`header__item ${
+                selectedItem === "education" ? "header__item--selected" : " "
+              }`}
+            >
+              Education
+            </li>
           </a>
           <li className="header__item header__item--last">Resume</li>
         </ul>
