ee9ee7738af1dc26a91cef3979d019fa706b8549
Maite Baida <maitemsabaida@gmail.com>
2024-07-03 11:31:13 -0600

    Education styles finished

--- src/components/Education/Education.jsx
+++ src/components/Education/Education.jsx
@@ -3,19 +3,31 @@ import "./Education.scss";
 function Education() {
   return (
     <section className="education">
-      <h2 className="education__header">Education</h2>
-      <ul>
-        <li>
-          <h3 className="education__subheader">Software Developer</h3>
-          <p className="education__txt">title</p>
-          <p className="education__txt">date, duration</p>
-        </li>
-        <li>
-          <h3 className="education__subheader">Architect and Urbanist</h3>
-          <p className="education__txt">title</p>
-          <p className="education__txt">date, duration</p>
-        </li>
-      </ul>
+      <div>
+        <h2 className="education__header">Education</h2>
+        <ul>
+          <li className="education__list-item">
+            <h3 className="education__subheader">
+              Full-Stack Software Developer
+            </h3>
+            <div>
+              <p className="education__txt">BrainStation</p>
+              <p className="education__txt">Canada (remote)</p>
+              <p className="education__txt">2023 - 2024</p>
+            </div>
+          </li>
+          <li className="education__list-item education__list-item--last">
+            <h3 className="education__subheader">Architect and Urbanist</h3>
+            <div>
+              <p className="education__txt">
+                FAAP - Fundacao Armando Alvares Penteado
+              </p>
+              <p className="education__txt">Sao Paulo, Brazil</p>
+              <p className="education__txt">2008 - 2012</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </section>
   );
 }
